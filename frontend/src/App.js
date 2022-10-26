8a23821bd8a6378580edbf10fad46c5d51a37eba
Jonghan Park <jonghan.park@edu.sait.ca>
2022-10-26 16:46:22 -0600

    Update router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Router>
+        <Routes>
+          <Route index element={<h1>Hello World</h1>} />
+          <Route path="movie/:id" element={<h1>Movie detail page</h1>} />
+          <Route path="movies/:type" element={<h1>Movies list page</h1>} />
+          <Route path="/*" element={<h1>Error page</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
