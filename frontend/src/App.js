8a05cae78c265856a2b60e2b9c95110e3f452889
Jonghan Park <jonghan.park@edu.sait.ca>
2022-10-26 17:00:37 -0600

    Update Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route index element={<h1>Hello World</h1>} />
           <Route path="movie/:id" element={<h1>Movie detail page</h1>} />
