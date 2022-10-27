de0a215027732128bd37bdb029ed26fc8b022dcc
Jonghan Park <jonghan.park@edu.sait.ca>
2022-10-27 16:53:29 -0600

    Update card

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "./card.css";
+import { Link } from "react-router-dom";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ movie }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  }, []);
+  return (
+    <>
+      {isLoading ? (
+        <div className="cards">
+          <SkeletonTheme color="#202020" highlightColor="#444">
+            <Skeleton height={300} duration={2} />
+          </SkeletonTheme>
+        </div>
+      ) : (
+        <Link
+          to={`movie/${movie.id}`}
+          style={{ textDecoration: "none", color: "white" }}
+        >
+          <div className="cards">
+            <img
+              src={`https://image.tmdb.org/t/p/origianl${
+                movie ? movie.original_title : ""
+              }`}
+              alt=""
+              className="cards__img"
+            />
+            <div className="cards__overlay">
+              <div className="cards__title">
+                {movie ? movie.original_title : ""}
+              </div>
+              <div className="cards__runtime">
+                {movie ? movie.release_data : ""}
+                <span className="card__rating">
+                  {movie ? movie.vote_average : ""}
+                  <i className="fas fa-star"></i>
+                </span>
+              </div>
+              <div className="cards__description">
+                {movie ? movie.overview.slice(0, 118) + "..." : ""}
+              </div>
+            </div>
+          </div>
+        </Link>
+      )}
+    </>
+  );
 };
 
 export default Card;
