af2942b8e3ea21c4f5b4e04986ec780107fe82ea
Jonghan Park <jonghan.park@edu.sait.ca>
2022-10-26 19:26:54 -0600

    Update main page

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 import "./home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const [popularMovies, setPopularMovies] = useState([]);
@@ -8,10 +11,56 @@ const Home = () => {
       "https://api.themoviedb.org/3/movie/popular?api_key=d35de3a48191599b0207ddf9c74cc52b"
     )
       .then((res) => res.json())
-      .then((data) => setPopularMovies(data.results));
+      .then((data) => setPopularMovies(data.results))
+      .then((data) => console.log(data));
   }, []);
 
-  return <>Homepage is now rendered</>;
+  return (
+    <>
+      <div className="poster">
+        <Carousel
+          showThumbs={false}
+          autoPlay={true}
+          transitionTime={3}
+          infiniteLoop={true}
+          showStatus={false}
+        >
+          {popularMovies.map((movie) => {
+            return (
+              <Link
+                style={{ TextDecoration: "none", color: "white" }}
+                to={`/movie/${movie.id}`}
+              >
+                <div className="posterImage">
+                  <img
+                    src={`https://image.tmdb.org/t/p/original${
+                      movie && movie.backdrop_path
+                    }`}
+                    alt="poster images"
+                  />
+                </div>
+                <div className="posterImage__overlay">
+                  <div className="posterImage__title">
+                    {movie ? movie.original_title : ""}
+                  </div>
+                  <div className="posterImage__runtime">
+                    {movie ? movie.release_date : ""}
+                    <span className="posterImage__rating">
+                      {movie ? movie.vote_average : ""}
+                      <i className="fas fa-star" />
+                    </span>
+                  </div>
+                  <div className="posterImage__description">
+                    {movie ? movie.overview : ""}
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
+        </Carousel>
+      </div>
+    </>
+  );
 };
 
 export default Home;
